65573e4fb179d3aeda3f03fdd2374b51c7eaa34d
colbycoleman731-oss <colby.coleman731@students.cmcss.net>
2025-12-17 11:21:39 -0600

    Update task1.py

--- task1.py
+++ task1.py
@@ -1,1 +1,23 @@
+# Part 1: Combine the haul
 
+total_candy = 59
+
+
+# Part 2: Fair sharing (itotal_candy = 59nclude yourself)
+Totalpeole = 3
+Totalshare= 19
+leftovercandy=3
+eachperson= 19
+leftover= 2
+
+
+# Part 3: Include the sick friend
+# Variable reassignment is fine - previous values were already printed
+Totalpeople = 4
+Totalcandyperperson = 18
+Totalleftover= 1
+Eachperson = 18
+Totalleftovers = 1
+# Week 1: You did dishes and cleaned your room
+youcleadedthedishes
+Youcleandyourroom
