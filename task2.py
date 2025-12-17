90d1c55626f9f43ba9a87952ab6361b5b10acc68
colbycoleman731-oss <colby.coleman731@students.cmcss.net>
2025-12-17 11:21:54 -0600

    Update task2.py

--- task2.py
+++ task2.py
@@ -1,1 +1,21 @@
+# Week 1: You did dishes and cleaned your room
+youcleadedthedishes
+Youcleandyourroom
 
+# You bought candy
+youboughtcandy
+
+# Week 2: Parents gave you a bonus! They doubled your allowance for working hard
+Bonus = allowance * 2
+
+# You mowed the lawn
+Youmowedhelawn
+
+# You bought a new game
+youboughtanewgame
+
+# Week 3: You decided to put half your money in savings
+savings = allowance/2
+
+# Print final allowance
+finalallowance = allowance * 2 / 2
