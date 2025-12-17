d80dcd942dd4dc7e10fa5591472bfda2ee8c9778
colbycoleman731-oss <colby.coleman731@students.cmcss.net>
2025-12-17 11:22:20 -0600

    Update task3.py

--- task3.py
+++ task3.py
@@ -1,1 +1,10 @@
-
+ITEM MENU:
+Invisibility Cloak.........$44.99
+Dragon Egg.....................$29.99
+newmenu=Flying Carpet - $119.99
+Phoenix Feather - $14.99
+Time Turner - $84.99
+Enchanted Sword - $65.99
+Potion of Luck - $11.99
+Crystal Ball - $39.99
+""")
